a6980d1886813cf037cb0484ca49341b531e82bb
David J. Burger <davidburger@gmail.com>
2023-04-05 19:09:42 -0500

    Links attempt to link to correct sport/league page

--- src/content-script.js
+++ src/content-script.js
@@ -1,6 +1,3 @@
-const button = document.createElement("button");
-button.appendChild(document.createTextNode("Click"));
-
 const findAnchor = (elem, stop) => {
   while (elem !== null && elem !== stop) {
     if (elem.tagName === "A") {
@@ -21,32 +18,54 @@ const findRow = (elem) => {
   return null;
 }
 
-const rowToUrl = (row, img) => {
-  return "http://www.etrade.com";
+const rowToUrl = (book, sport, league) => {
+  if (book === "Barstool") {
+    return `https://www.barstoolsportsbook.com/sports/${sport.toLowerCase()}/${league.toLowerCase()}`;
+  } else if (book === "BetMGM") {
+    return `https://sports.ks.betmgm.com/en/sports`;
+  } else if (book === "Caesars") {
+    return `https://sportsbook.caesars.com/us/ks/bet/${sport.toLowerCase()}/events/all`;
+  } else if (book === "DraftKings") {
+    return `https://sportsbook.draftkings.com/leagues/${sport.toLowerCase()}/${league.toLowerCase()}`
+  } else if (book === "FanDuel") {
+    return `https://sportsbook.fanduel.com/navigation/${league.toLowerCase()}`;
+  } else if (book === "PointsBet (Kansas)") {
+    return `https://ks.pointsbet.com/sports/basketball/${league.toUpperCase()}`;
+  } else {
+    const term = "Celtics";
+    return `https://www.pinnacle.com/en/search/${term}`;
+  }
 }
+
+const button = document.createElement("button");
+button.appendChild(document.createTextNode("Linkify"));
 
 button.addEventListener("click", (evt) => {
   const imgs = document.querySelectorAll("img");
   for (const img of imgs) {
     const row = findRow(img);
     if (row !== null) {
-      img.addEventListener("click", (evt) => {
-        // evt.preventDefault();
-      });
-      const a = findAnchor(img, row);
-      if (a !== null) {
-        a.setAttribute("href", rowToUrl(row, img));
-      } else {
-        const a = document.createElement("a");
-        a.setAttribute("href", rowToUrl(row, img));
-        const parent = img.parentNode;
-        parent.removeChild(img);
-        a.appendChild(img);
-        parent.appendChild(a);
+      const anchor = findAnchor(img, row);
+      if (anchor) {
+        anchor.setAttribute("href", "#");
       }
+      img.style.cursor = "pointer";
+      img.style.border = "1px solid red";
+      img.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        const book = img.alt;
+        const event = row.children.item(4).children.item(1).children.item(1).textContent;
+        const parts = event.split(" | ");
+        const sport = parts[0];
+        const league = parts[1];
+        const url = rowToUrl(book, sport, league);
+        window.open(url, "_blank");
+      });
     }
   }
 });
 
+
+// const logo = document.querySelector('img[alt="OddsJam"]')
+// logo.parentNode.appendChild(button);
 document.body.appendChild(button);
-console.log("hooked");
